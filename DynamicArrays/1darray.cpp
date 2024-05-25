730cb0a1de3da04766ffc3dc02692ab06bbfc01a
Anant Gyan Singhal <168709064+lethalsage@users.noreply.github.com>
2024-05-25 07:51:12 +0530

    1d array methods

---
+++ DynamicArrays/1darray.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+int main() {
+    int size;
+    std::cout << "Enter the size of the array: ";
+    std::cin >> size;
+
+    int* dynamicArray = new int[size];
+
+    // Access and modify elements of the dynamic array
+    for (int i = 0; i < size; i++) {
+        dynamicArray[i] = i + 1;
+    }
+
+    // Print the elements of the dynamic array
+    for (int i = 0; i < size; i++) {
+        std::cout << dynamicArray[i] << " ";
+    }
+
+    // Don't forget to deallocate the memory
+    delete[] dynamicArray;
+
+    return 0;
+}
+
+
+or
+// to make a array of size 10
+std::array<int, 10> array;
